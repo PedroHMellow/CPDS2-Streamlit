a480b349e5e9a87b5b018a5b75c4e5f0ffb849f8
GustavoG <126207720+OlaMeusAmigues@users.noreply.github.com>
2025-04-10 21:52:05 -0300

    modified:   App.py 	modified:   Page/Intervalo.py

--- App.py
+++ App.py
@@ -1,4 +1,6 @@
+import pandas as pd
 import streamlit as st
+from datetime import datetime
 from pathlib import Path
 from streamlit_option_menu import option_menu
 
@@ -6,6 +8,43 @@ from streamlit_option_menu import option_menu
 # Configurações da Pagina 
 TPagina = "CP2DS | Dashboards"
 st.set_page_config(page_title=TPagina, layout="wide")
+
+
+# Armazenar banco de dados tratado no Streamlit
+if "df_ig" not in st.session_state:
+        # leitura do excel
+        df = pd.read_excel("instagram_completo_formatado.xlsx", index_col=False)
+
+        # criar colunas de Dia da Semana e Horário do Post
+        datahora = df["Hora de publicação"].to_list()
+        dia_da_semana = []
+        hora_do_post = []
+        semana_dict = {
+            "Sun":"Domingo",
+            "Mon":"Segunda-Feira",
+            "Tue":"Terça-Feira",
+            "Wed":"Quarta-Feira",
+            "Thu":"Quinta-Feira",
+            "Fri":"Sexta-Feira",
+            "Sat":"Sábado",
+        }
+        for data in datahora:
+            data_datetime = datetime.strptime(data, "%m/%d/%Y %H:%M")
+            dia_da_semana.append( f"{semana_dict[data_datetime.strftime("%a")]}" )
+            hora_do_post.append(int(f"{data_datetime.strftime("%H")}"))
+        dia_da_semana_df = pd.DataFrame({"Dia da Semana do Post":dia_da_semana})
+        hora_do_post_df = pd.DataFrame({"Horário de Postagem":hora_do_post})
+        df["Dia da Semana do Post"] = dia_da_semana_df
+        df["Horário de Postagem"] = hora_do_post_df
+
+
+        # Renomear colunas necessárias
+        df.rename(columns={'Gostos': 'Curtidas', 'Partilhas': 'Compartilhamentos','Seguimentos': 'Novos Seguidores', 'Itens guardados': 'Vídeos Salvos', "Descrição":"Nome dos Posts", "Ligação permanente":"Link", "Tipo de publicação":"Tipo de Publicação"}, inplace=True)
+        # Remover colunas indesejadas e Organizar Restantes
+        df = df[["Nome dos Posts", "Link", "Duração (segundos)", "Horário de Postagem", "Dia da Semana do Post", "Tipo de Publicação", "Alcance", 'Curtidas', 'Compartilhamentos', 'Novos Seguidores', "Comentários", "Vídeos Salvos", "Visualizações"]]
+        df.drop(axis="index", index=[0], inplace=True)
+
+        st.session_state["df_ig"] = df
 
 
 #css
@@ -22,9 +61,9 @@ with st.sidebar:
         f"""
         <div style="text-align: center;">
                 <img src="https://cdn-icons-png.flaticon.com/512/174/174855.png" 
-                     style="width: 180px; height: 180px; border-radius: 50%; object-fit: cover;">
+                     style="width: 50px; height: 50px; border-radius: 50%; object-fit: cover;">
             </a>
-            <p style="margin-top: 10px; font-size: 16px; font-weight: bold;">Análise Instagram</p>
+            <p style="margin-top: 10px; font-size: 20px; font-weight: bold;">Análise Instagram</p>
         </div>
         """,
         unsafe_allow_html=True
--- Page/Intervalo.py
+++ Page/Intervalo.py
@@ -1,4 +1,355 @@
+import numpy as np
 import streamlit as st
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+from scipy import stats
 
 def show():
-    st.title("📈Intervalo de Confiança")
+    st.write("# Tabela IG para consulta:")
+    st.divider()
+    st.dataframe(st.session_state["df_ig"])
+    st.write(" ")
+    st.write(" ")
+
+
+
+    st.write("# Intervalo de Confiança:")
+    st.divider()
+    col1, col2, col3 = st.columns([4, 3, 3])
+
+    col1.write("##### Valor a ser analisado:")
+    valor_dado = col1.selectbox(
+        label="",
+        options=("Alcance", "Curtidas", "Compartilhamentos", "Novos Seguidores", "Comentários", "Vídeos Salvos", "Visualizações"),
+        index = None,
+        placeholder="Classificar dados por..."
+                                )
+    col1.write(" ")
+    col1.write(" ")
+
+    col2.write("##### Como nomear os dados:")
+    nome_dado = col2.selectbox(
+        label="",
+        options=("Nome dos Posts", "Duração (segundos)", "Horário de Postagem", "Dia da Semana do Post", "Tipo de Publicação"),
+        index = None,
+        placeholder="Nomear os dados por..."
+                                )
+    col2.write(" ")
+    col2.write(" ")
+
+    col3.write("##### Valor de Confiança:")
+    confianca_dado = col3.selectbox(
+        label="",
+        options=(0.90, 0.95, 0.99),
+        index = None,
+        placeholder="Confiabilidade do dado..."
+                                )
+    col3.write(" ")
+    col3.write(" ")
+
+
+
+    if not valor_dado or not nome_dado or not confianca_dado:
+        col1, col2, col3 = st.columns([4, 3, 3])
+        col2.write(" ")
+        col2.write("## ❗Selecionar:")
+        if not valor_dado:
+            col2.write("- ❌Valor a ser analisado")
+        else:
+            col2.write(f"- ✅Valor a ser analisado : {valor_dado}")
+        if not nome_dado:
+            col2.write("- ❌Como nomear os dados")
+        else:
+            col2.write(f"- ✅Como nomear os dados : {nome_dado}")
+        if not confianca_dado:
+            col2.write("- ❌Valor de Confiança")
+        else:
+            col2.write(f"- ✅Valor de Confiança : {confianca_dado}")
+    else:
+        # coluna específica e sem valores nulos
+        coluna_escolhida = st.session_state["df_ig"][[valor_dado, nome_dado]].dropna()
+
+        # retirar imagens
+        if nome_dado == "Duração (segundos)":
+            coluna_escolhida = coluna_escolhida[coluna_escolhida[nome_dado] > 0]
+            
+        # descrição da coluna escolhida
+        amostra_descricao = coluna_escolhida[valor_dado].describe()
+        amostra_descricao.rename(index={'count': 'Contagem', 'mean': 'Média', "std":"Desvio Padrão"}, inplace=True)
+        st.divider()
+        st.write("### Abordagem sem Valores Nulos")
+
+
+
+        col1, col2 = st.columns([7, 3])
+
+        if nome_dado == "Nome dos Posts":
+            col2.write(" ")
+            col2.write(" ")
+            col2.write("###### Descrição da Amostra:")
+            col2.dataframe(amostra_descricao)
+
+            media = amostra_descricao['Média']
+            sem = amostra_descricao["Desvio Padrão"]
+            ic = stats.t.interval(confianca_dado, len(coluna_escolhida[valor_dado])-1, loc=media, scale=sem)
+            erro_superior = ic[1] - media
+            erro_inferior = media - ic[0]
+            if media - erro_inferior < 0:
+                erro_inferior = media
+            fig_intervalo_antigo = go.Figure()
+            fig_intervalo_antigo.add_trace(go.Scatter(
+                x=[nome_dado],
+                y=[media],
+                error_y=dict(
+                    type='data',
+                    array=[erro_superior],
+                    arrayminus=[erro_inferior],
+                    visible=True,
+                    thickness=2,
+                    color='rgb(189, 89, 89)'
+                ),
+                mode='markers',
+                marker=dict(size=10, color='red')
+            ))
+            fig_intervalo_antigo.update_layout(
+                title=f"Média de {valor_dado} com Intervalo de Confiança de {confianca_dado*100}%",
+                yaxis_title="Média",
+                showlegend=False
+            )
+            col1.plotly_chart(fig_intervalo_antigo, key="apresentação antigo grupo")
+        else:
+            df_divisoes = pd.DataFrame(columns=["Divisão","Média", "Desvio Padrão", "Limite Superior", "Limite Inferior"])
+            lista_tipo = None
+            lista_tipo_intervalo = None
+            if nome_dado == "Duração (segundos)":
+                lista_tipo = None
+                lista_tipo_intervalo = [(1, 15), (15, 30), (30,45), (45,90)]
+            elif nome_dado == "Horário de Postagem":
+                lista_tipo = None
+                lista_tipo_intervalo = [(0, 13), (13, 14), (14,19)]
+            elif nome_dado == "Dia da Semana do Post":
+                lista_tipo = ["Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "Sábado"]
+                lista_tipo_intervalo = None
+            # Tipo de Publicação
+            else:
+                lista_tipo = ["Carrossel do Instagram", "Imagem do Instagram", "Reel do IG"]
+                lista_tipo_intervalo = None
+                
+            if lista_tipo is not None:
+                for tipo in lista_tipo:
+                    divisao = coluna_escolhida[coluna_escolhida[nome_dado] == tipo]
+                    media = np.mean(divisao[valor_dado])
+                    sem = stats.sem(divisao[valor_dado])
+                    ic = stats.t.interval(confianca_dado, len(divisao[valor_dado])-1, loc=media, scale=sem)
+                    erro_superior = ic[1] - media
+                    erro_inferior = media - ic[0]
+                    if media - erro_inferior < 0:
+                        erro_inferior = media
+                    if erro_superior > 0:
+                        df_divisao = pd.DataFrame({"Divisão":tipo, "Média":media, "Desvio Padrão":sem, "Limite Superior":erro_superior, "Limite Inferior":erro_inferior}, index=[0])
+                        df_divisoes = pd.concat([df_divisoes, df_divisao], ignore_index=True)
+                        col2.write(tipo)
+                        descricao_tipo = divisao[valor_dado].describe()
+                        descricao_tipo = descricao_tipo.loc[["count", "max", "mean", "min"]]
+                        amostra_descricao.rename(index={'count': 'Contagem', 'mean': 'Média'}, inplace=True)
+                        col2.dataframe(descricao_tipo)
+            if lista_tipo_intervalo is not None:
+                for tipo in lista_tipo_intervalo:
+                    divisao = coluna_escolhida[coluna_escolhida[nome_dado] >= tipo[0]]
+                    divisao = divisao[divisao[nome_dado] < tipo[1]]
+                    media = np.mean(divisao[valor_dado])
+                    sem = stats.sem(divisao[valor_dado])
+                    ic = stats.t.interval(confianca_dado, len(divisao[valor_dado])-1, loc=media, scale=sem)
+                    erro_superior = ic[1] - media
+                    erro_inferior = media - ic[0]
+                    if media - erro_inferior < 0:
+                        erro_inferior = media
+                    if erro_superior > 0:
+                        df_divisao = pd.DataFrame({"Divisão":f"{tipo[0]}-{tipo[1]}", "Média":media, "Desvio Padrão":sem, "Limite Superior":erro_superior, "Limite Inferior":erro_inferior}, index=[0])
+                        df_divisoes = pd.concat([df_divisoes, df_divisao], ignore_index=True)
+                        col2.write(f"{tipo[0]}-{tipo[1]}")
+                        descricao_tipo = divisao[valor_dado].describe()
+                        descricao_tipo = descricao_tipo.loc[["count", "max", "mean", "min"]]
+                        amostra_descricao.rename(index={'count': 'Contagem', 'mean': 'Média'}, inplace=True)
+                        col2.dataframe(descricao_tipo)
+                
+            fig_intervalo_antigo = go.Figure()
+            fig_intervalo_antigo.add_trace(
+                go.Scatter(
+                    x=df_divisoes["Divisão"],
+                    y=df_divisoes["Média"],
+                    error_y=dict(
+                        type='data',
+                        array=df_divisoes["Limite Superior"],
+                        arrayminus=df_divisoes["Limite Inferior"],
+                        visible=True,
+                        thickness=2,
+                        color='rgb(189, 89, 89)'
+                    ),
+                    mode='markers',
+                    marker=dict(size=10, color='red')
+                )
+            )
+            fig_intervalo_antigo.update_layout(
+                title=f"Média de {valor_dado} com Intervalo de Confiança de {confianca_dado*100}%",
+                yaxis_title="Média",
+                xaxis_title=nome_dado,
+                hovermode="x"
+            )
+            col1.plotly_chart(fig_intervalo_antigo, key="apresentação antigo grupo")
+            col1.write(" ")
+            col1.write(" ")
+            col1.write("###### Descrição da Amostra:")
+            col1.dataframe(amostra_descricao)
+        st.session_state["imagem_intervalo_antigo"] = fig_intervalo_antigo
+
+        fig = px.box(coluna_escolhida, y=valor_dado, points="all", hover_data={nome_dado: True}, title="Boxplot da Amostra")
+        fig.update_traces(marker=dict(size=10, color="red", opacity=0.7), jitter=0.3)
+        col1.plotly_chart(fig)
+
+
+        
+        q1 = coluna_escolhida[valor_dado].quantile(0.25)
+        q3 = coluna_escolhida[valor_dado].quantile(0.75)
+        iqr = q3 - q1
+        # limite superior para retira outliers
+        limite_superior = q3 + 1.5*iqr
+        # coluna específica, sem valores nulos e sem outliers
+        coluna_escolhida_reduzida = coluna_escolhida[coluna_escolhida[valor_dado] <= limite_superior]
+        st.write(f"### Abordagem sem Valores Nulos e antigos Outliers(antigo Limite Superior: {limite_superior})")
+        # descrição da coluna escolhida reduzido
+        amostra_reduzida_descricao = coluna_escolhida_reduzida[valor_dado].describe()
+        amostra_reduzida_descricao.rename(index={'count': 'Contagem', 'mean': 'Média', "std":"Desvio Padrão"}, inplace=True)
+        col1, col2 = st.columns([7, 3])
+
+        if nome_dado == "Nome dos Posts":
+            col2.write(" ")
+            col2.write(" ")
+            col2.write("###### Descrição da Amostra:")
+            col2.dataframe(amostra_reduzida_descricao)
+
+            media = amostra_reduzida_descricao['Média']
+            sem = amostra_reduzida_descricao["Desvio Padrão"]
+            ic = stats.t.interval(confianca_dado, len(coluna_escolhida_reduzida[valor_dado])-1, loc=media, scale=sem)
+            erro_superior = ic[1] - media
+            erro_inferior = media - ic[0]
+            if media - erro_inferior < 0:
+                erro_inferior = media
+            fig_intervalo_novo = go.Figure()
+            fig_intervalo_novo.add_trace(go.Scatter(
+                x=[nome_dado],
+                y=[media],
+                error_y=dict(
+                    type='data',
+                    array=[erro_superior],
+                    arrayminus=[erro_inferior],
+                    visible=True,
+                    thickness=2,
+                    color='rgb(189, 89, 89)'
+                ),
+                mode='markers',
+                marker=dict(size=10, color='red')
+            ))
+            fig_intervalo_novo.update_layout(
+                title=f"Média de {valor_dado} com Intervalo de Confiança de {confianca_dado*100}%",
+                yaxis_title="Média",
+                showlegend=False
+            )
+            col1.plotly_chart(fig_intervalo_novo, key="apresentação novo único")
+        else:
+            df_divisoes = pd.DataFrame(columns=["Divisão","Média", "Desvio Padrão", "Limite Superior", "Limite Inferior"])
+            lista_tipo = None
+            lista_tipo_intervalo = None
+            if nome_dado == "Duração (segundos)":
+                lista_tipo = None
+                lista_tipo_intervalo = [(1, 15), (15, 30), (30,45), (45,90)]
+            elif nome_dado == "Horário de Postagem":
+                lista_tipo = None
+                lista_tipo_intervalo = [(0, 13), (13, 14), (14,19)]
+            elif nome_dado == "Dia da Semana do Post":
+                lista_tipo = ["Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "Sábado"]
+                lista_tipo_intervalo = None
+            # Tipo de Publicação
+            else:
+                lista_tipo = ["Carrossel do Instagram", "Imagem do Instagram", "Reel do IG"]
+                lista_tipo_intervalo = None
+                
+            if lista_tipo is not None:
+                for tipo in lista_tipo:
+                    divisao = coluna_escolhida_reduzida[coluna_escolhida_reduzida[nome_dado] == tipo]
+                    media = np.mean(divisao[valor_dado])
+                    sem = stats.sem(divisao[valor_dado])
+                    ic = stats.t.interval(confianca_dado, len(divisao[valor_dado])-1, loc=media, scale=sem)
+                    erro_superior = ic[1] - media
+                    erro_inferior = media - ic[0]
+                    if media - erro_inferior < 0:
+                        erro_inferior = media
+                    if erro_superior > 0:
+                        df_divisao = pd.DataFrame({"Divisão":tipo, "Média":media, "Desvio Padrão":sem, "Limite Superior":erro_superior, "Limite Inferior":erro_inferior}, index=[0])
+                        df_divisoes = pd.concat([df_divisoes, df_divisao], ignore_index=True)
+                        col2.write(tipo)
+                        descricao_tipo = divisao[valor_dado].describe()
+                        descricao_tipo = descricao_tipo.loc[["count", "max", "mean", "min"]]
+                        amostra_reduzida_descricao.rename(index={'count': 'Contagem', 'mean': 'Média'}, inplace=True)
+                        col2.dataframe(descricao_tipo)
+            if lista_tipo_intervalo is not None:
+                for tipo in lista_tipo_intervalo:
+                    divisao = coluna_escolhida_reduzida[coluna_escolhida_reduzida[nome_dado] >= tipo[0]]
+                    divisao = divisao[divisao[nome_dado] < tipo[1]]
+                    media = np.mean(divisao[valor_dado])
+                    sem = stats.sem(divisao[valor_dado])
+                    ic = stats.t.interval(confianca_dado, len(divisao[valor_dado])-1, loc=media, scale=sem)
+                    erro_superior = ic[1] - media
+                    erro_inferior = media - ic[0]
+                    if media - erro_inferior < 0:
+                        erro_inferior = media
+                    if erro_superior > 0:
+                        df_divisao = pd.DataFrame({"Divisão":f"{tipo[0]}-{tipo[1]}", "Média":media, "Desvio Padrão":sem, "Limite Superior":erro_superior, "Limite Inferior":erro_inferior}, index=[0])
+                        df_divisoes = pd.concat([df_divisoes, df_divisao], ignore_index=True)
+                        col2.write(f"{tipo[0]}-{tipo[1]}")
+                        descricao_tipo = divisao[valor_dado].describe()
+                        descricao_tipo = descricao_tipo.loc[["count", "max", "mean", "min"]]
+                        amostra_reduzida_descricao.rename(index={'count': 'Contagem', 'mean': 'Média'}, inplace=True)
+                        col2.dataframe(descricao_tipo)
+                
+            fig_intervalo_novo = go.Figure()
+            fig_intervalo_novo.add_trace(
+                go.Scatter(
+                    x=df_divisoes["Divisão"],
+                    y=df_divisoes["Média"],
+                    error_y=dict(
+                        type='data',
+                        array=df_divisoes["Limite Superior"],
+                        arrayminus=df_divisoes["Limite Inferior"],
+                        visible=True,
+                        thickness=2,
+                        color='rgb(189, 89, 89)'
+                    ),
+                    mode='markers',
+                    marker=dict(size=10, color='red')
+                )
+            )
+            fig_intervalo_novo.update_layout(
+                title=f"Média de {valor_dado} com Intervalo de Confiança de {confianca_dado*100}%",
+                yaxis_title="Média",
+                xaxis_title=nome_dado,
+                hovermode="x"
+            )
+            col1.plotly_chart(fig_intervalo_novo, key="apresentação novo grupo")
+            col1.write(" ")
+            col1.write(" ")
+            col1.write("###### Descrição da Amostra:")
+            col1.dataframe(amostra_reduzida_descricao)
+        st.session_state["imagem_intervalo_novo"] = fig_intervalo_novo
+
+        fig = px.box(coluna_escolhida_reduzida, y=valor_dado, points="all", hover_data={nome_dado: True}, title="Boxplot da Amostra")
+        fig.update_traces(marker=dict(size=10, color="red", opacity=0.7), jitter=0.3)
+        col1.plotly_chart(fig)
+
+
+
+        st.write("### Abordagem Antiga X Abordagem Nova")
+        col1, col2 = st.columns(2)
+        col1.plotly_chart(st.session_state["imagem_intervalo_antigo"], key="versus antigo")
+        col2.plotly_chart(st.session_state["imagem_intervalo_novo"], key="versus novo")
